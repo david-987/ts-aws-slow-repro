fee85469c56b950d74a1533abf98bd76097a8458
david-stipend <davidwho@gmail.com>
2022-07-15 01:04:15 +0800

    more pain

--- src/packages/aws/src/creds.ts
+++ src/packages/aws/src/creds.ts
@@ -4,7 +4,11 @@ import {
     SharedIniFileCredentials,
     EC2MetadataCredentials,
 } from 'aws-sdk/global';
+import { ExpressReceiver } from '@slack/bolt';
+import slack from '@slack/web-api';
 
+let a = new slack.WebClient();
+let b = new ExpressReceiver({ signingSecret: undefined});
 CredentialProviderChain.defaultProviders = [
 
     () => new EnvironmentCredentials('AWS'),
