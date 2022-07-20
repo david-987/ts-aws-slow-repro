09f700628589146685a8dafe3a15f555eb6c88c6
david-stipend <davidwho@gmail.com>
2022-07-20 11:09:06 +0800

    simplify

--- src/client/pages/Test.tsx
+++ src/client/pages/Test.tsx
@@ -1,15 +1,7 @@
 import React from 'react';
 import { UtiLClass } from '@packages/util';
 import { UserClass } from '@packages/user';
-import { UserClass as UserClass2  } from '@packages/user2';
-import { UserClass as UserClass3} from '@packages/user3';
-import { UserClass as UserClass4} from '@packages/user4';
-import { UserClass as UserClass5} from '@packages/user5';
-import { UserClass as UserClass6} from '@packages/user6';
-import { UserClass as UserClass7} from '@packages/user7';
-import { UserClass as UserClass8} from '@packages/user8';
-import { UserClass as UserClass9} from '@packages/user9';
-import MyModel from 'server/model'
+import MyModel from 'server/product/model';
 
 import Component from 'client/components/TestComponent';
 
@@ -18,16 +10,14 @@ interface Props {
 }
 
 const Page = ({ prop }: Props): React.ReactElement => {
-    return (
-        <Component />
-    );
+    return <Component />;
 };
 
 Page.getInitialProps = async ({ req }) => {
     const util = new UtiLClass();
-    const r= util.test();
+    const r = util.test();
     return {
-        prop: r
+        prop: r,
     };
 };
 
--- src/packages/user/src/operations/UserClass.ts
+++ src/packages/user/src/operations/UserClass.ts
@@ -1,13 +1,14 @@
 import S3 from 'aws-sdk/clients/s3';
-import rekognition from 'aws-sdk/clients/rekognition';
-import { UserClass as UserClass2 } from '@packages/user';
+import rekognition from 'aws-sdk/clients/rekognition'
 
-import { UtiLClass } from '@packages/util';
+import {UtiLClass} from '@packages/util';
 import { ExpressReceiver, UnknownError } from '@slack/bolt';
 
 const s3 = new S3({
     credentialProvider: undefined,
 });
+
+
 
 class UserClass {
     constructor() {}
@@ -15,10 +16,10 @@ class UserClass {
     async loader() {
         s3.deleteBucket();
         s3.headBucket();
-        s3.upload({ Bucket: '', Key: '' });
+        s3.upload({ Bucket: '', Key: ''});
         const util = new UtiLClass();
         const recog = new rekognition();
-        const receiver = new ExpressReceiver({ signingSecret: '' });
+        const receiver = new ExpressReceiver({ signingSecret: ''});
         await util.test();
     }
 }
